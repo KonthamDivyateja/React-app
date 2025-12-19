8ccf62b1d684285a89b560e283fdd0627c939f62
KonthamDivyateja <139646440+KonthamDivyateja@users.noreply.github.com>
2025-12-19 23:57:11 +0530

    Add files via upload

---
+++ counter.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count !== 0 && count % 3 === 0) {
+      alert(`The current number ${count} is divisible by 3`);
+    }
+  }, [count]);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <h3>Count: {count}</h3>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
